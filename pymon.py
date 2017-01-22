98f216075e64f340d6bdce8443ac8be8a1ae4d8d
zheka <poliv78@gmail.com>
2017-01-22 21:10:36 +0200

    added disk check

--- pymon.py
+++ pymon.py
@@ -4,6 +4,7 @@ import psutil
 #------------------------alert settings-------------------------
 free_mem = 10  # percents
 free_cpu = 10  # percents
+free_space = 5  # percents
 # #---------------------------------------------------------
 
 alert_messages = []
@@ -16,4 +17,10 @@ mem = psutil.virtual_memory()
 if mem.available*100/mem.total <= free_mem:
     alert_messages.append("Free memory is less than " + str(free_mem) + "%")
 
+disks = psutil.disk_partitions()
+for disk in disks:
+    disk_usage = psutil.disk_usage(disk.mountpoint)
+    if (100 - disk_usage.percent) <= free_space:
+        alert_messages.append("Partition " + str(disk.mountpoint) + " has " +
+            str(100 - disk_usage.percent) + "% free space left")
 print alert_messages
