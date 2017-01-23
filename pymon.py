3f4946b61e4765e43b48ea51bb1b2c9a87e76db6
zheka <poliv78@gmail.com>
2017-01-23 13:05:06 +0200

    Processes check slack notification

--- pymon.py
+++ pymon.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 import psutil
+import requests
+
 
 #------------------------alert settings-------------------------
+server_name = "server"
 free_mem = 10  # percents
 free_cpu = 10  # percents
 free_space = 5  # percents
+procs = []
+webhook_url = 'https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 # #---------------------------------------------------------
+
+
+def slack():
+    txt = ""
+    for alert in alert_messages:
+        txt = txt + alert + " on "+server_name+". \n"
+    slack_data = '{"text":"'+txt+'", "channel":"#alerts"}'
+    requests.post(
+        webhook_url, data=slack_data,
+        headers={'Content-Type': 'application/json'}
+    )
+    return
+
 
 alert_messages = []
 cpus = psutil.cpu_percent(interval=1, percpu=True)
@@ -23,4 +41,19 @@ for disk in disks:
     if (100 - disk_usage.percent) <= free_space:
         alert_messages.append("Partition " + str(disk.mountpoint) + " has " +
             str(100 - disk_usage.percent) + "% free space left")
+
+for proc in psutil.process_iter():
+    try:
+        pinfo = proc.as_dict(attrs=['pid', 'name'])
+    except psutil.NoSuchProcess:
+        pass
+    else:
+        for p in procs:
+            if (p == pinfo["name"] or p in pinfo["name"]) and p in procs:
+                procs.remove(p)
+for p in procs:
+    alert_messages.append("Process " + p + " is dead")
+
 print alert_messages
+slack()
+
