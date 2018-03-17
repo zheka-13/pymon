1ae0e57109a6f0e60368082ebdcf1bd384de42d5
Zheka_13 <poliv78@gmail.com>
2018-03-17 19:34:13 +0200

    added carbon metrics

--- pymon.py
+++ pymon.py
@@ -2,15 +2,21 @@
 import psutil
 import requests
 import socket
+import time
+import sys
 
 #------------------------alert settings-------------------------
+CARBON_SERVER = 'carbon.example.net'
+CARBON_PORT = 2003
 server_name = socket.gethostname()
 free_mem = 10  # percents
 free_cpu = 10  # percents
 free_space = 5  # percents
-procs = []
-webhook_url = 'https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+procs = ['lvp', 'cron', 'apache', 'pgbouncer', 'redis', 'stun']
+webhook_url = 'https://hooks.slack.com/XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 # #---------------------------------------------------------
+metric_server = "db"
+
 
 
 def slack():
@@ -24,14 +30,35 @@ def slack():
     )
     return
 
+def send_metrics(m):
+    if (len(m)>0):
+	timestamp = int(time.time())
+	message = "";
+	for mes in m:
+	    message = message + mes + " "+ str(timestamp) + "\n"
+	sock = socket.socket()
+	sock.connect((CARBON_SERVER, CARBON_PORT))
+	sock.sendall(message)
+	sock.close()
 
+
+
+metrics = []
 alert_messages = []
+
+cpu_data =  psutil.cpu_times_percent(interval=1)
+metrics.append("host."+metric_server+".cpu.user "+str(cpu_data.user))
+metrics.append("host."+metric_server+".cpu.system "+str(cpu_data.system))
+metrics.append("host."+metric_server+".cpu.iowait "+str(cpu_data.iowait))
+
 cpus = psutil.cpu_percent(interval=1, percpu=True)
 for cpu in cpus:
     if cpu >= (100 - free_cpu):
         alert_messages.append("One of CPU cores usage is " + str(cpu) + "%")
 
 mem = psutil.virtual_memory()
+metrics.append("host."+metric_server+".memory.used "+str(mem.percent))
+
 if mem.available*100/mem.total <= free_mem:
     alert_messages.append("Free memory is less than " + str(free_mem) + "%")
 
@@ -41,6 +68,30 @@ for disk in disks:
     if (100 - disk_usage.percent) <= free_space:
         alert_messages.append("Partition " + str(disk.mountpoint) + " has " +
             str(100 - disk_usage.percent) + "% free space left")
+
+
+lvp_procs = {'total' : 0, 'op' : 0, 'dlr' : 0, 'calls' : 0}
+for proc in psutil.process_iter(attrs=['cmdline', 'name']):
+    if proc.info['name'] == 'lvp':
+	if (len(proc.info['cmdline'])==1):
+	    cmd = proc.info['cmdline'][0]
+	elif (len(proc.info['cmdline'])==2):
+	    cmd = proc.info['cmdline'][1]
+	else:
+	    cmd = proc.info['cmdline'][0]
+	lvp_procs['total'] +=1
+	if "WM_Conn" in cmd:
+	    lvp_procs['op'] +=1
+	elif "#comp" in cmd:
+	    lvp_procs['dlr'] +=1
+	elif "lvp_" in cmd:
+	    continue 
+	else:
+	    lvp_procs['calls'] +=1
+
+metrics.append("host."+metric_server+".procs.total "+str(lvp_procs['total']))
+metrics.append("host."+metric_server+".procs.calls "+str(lvp_procs['calls']))
+
 
 for proc in psutil.process_iter():
     try:
@@ -54,5 +105,6 @@ for proc in psutil.process_iter():
 for p in procs:
     alert_messages.append("Process " + p + " is dead")
 
+print alert_messages
 slack()
-
+send_metrics(metrics)
